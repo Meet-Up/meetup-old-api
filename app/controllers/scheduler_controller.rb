fd8173da402d98d6c6962c605304de1be98cf55c
Yuya Soneoka <ej.a9p96@gmail.com>
2013-09-01 14:38:40 +0900

    change color of web api

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -1,14 +1,14 @@
 class SchedulerController < ApplicationController
   def respondToToken
-    @dates=["1/1","1/2","1/3","1/4","1/5"]
+    @dates=["1/1","1/2","1/3"]
     @s_time=11.5
     @e_time=22
     @rows = (@e_time - @s_time) * 2 -1
     @cols = @dates.length - 1
     @cellWidth = [870/(@cols+1),270].min
     @cellHeight =[1200/(@rows+1),100].max
-    @unselectedColor = "rgb(224,223,177)"
-    @selectedColor = "rgb(149, 31, 43)"
+    @unselectedColor = "#DCDDDD"
+    @selectedColor = "#E2EEC5"
     @name = "David Liu's mock nomikai"
     @description="hoge"
 
