046554015531c794ee06d00a8a23143988148e5a
akira.saso <sasoakira6114@gmail.com>
2013-09-01 14:22:11 +0900

    c

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,25 +3,27 @@ class EventsController < ApplicationController
 
   before_filter :auth_user!
 
-	def create
-    @event = @user.created_events.build(params[:event])
-		if @event.save
-			users = params[:user]
-      unless users.nil?
-        users.each { |mail|
-          u = User.where(email: mail).first
-          if u.nil? then
-            u = User.create(:email => mail)
-          end
-          secure = SecureRandom.urlsafe_base64(TOKEN_LENGTH, false)
-          EventToken.create(:event_id => @event.id, :user_id => u.id, :token => secure)
-        }
-      end
-			render json:  @event
-		else
-			render :json  => @event.errors
-		end
-	end
+  def create
+	  @event = @user.created_events.build(params[:event])
+	  if @event.save
+		  users = params[:user]
+		  unless users.nil?
+			  users.each { |mail|
+				  u = User.where(email: mail).first
+				  if u.nil? then
+					  u = User.create(:email => mail)
+				  end
+				  secure = SecureRandom.urlsafe_base64(TOKEN_LENGTH, false)
+				  EventToken.create(:event_id => @event.id, :user_id => u.id, :token => secure)
+			  }
+			  secure = SecureRandom.urlsafe_base64(TOKEN_LENGTH, false)
+			  EventToken.create(:event_id => @event.id, :token => secure)
+		  end
+		  render json:  @event
+	  else
+		  render :json  => @event.errors
+	  end
+  end
 
 	def show
 		@event = Event.find(params[:id])
