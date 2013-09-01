20d032c8dafa465900ab5894a69d85cefa1c7ed7
akira.saso <sasoakira6114@gmail.com>
2013-09-01 15:26:04 +0900

    c

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -19,12 +19,15 @@ class SchedulerController < ApplicationController
 	  @s_time=time_tmp.min.to_i
 	  @e_time=time_tmp.max.to_i
 
+
+	  @name = e.event.name
+	  @description=e.event.description
+
+
 	  #puts YAML::dump(time_tmp)
 	  #puts YAML::dump(time_tmp.max)
 	  #puts YAML::dump(time_tmp.min)
 	  #puts YAML::dump(time_tmp)
-	  #puts YAML::dump("bbbbb")
-	  #puts YAML::dump("bbbbb")
 
 	  #@dates=["1/1","1/2","1/3","1/4","1/5"]
 	  #@s_time=10.5
@@ -37,8 +40,6 @@ class SchedulerController < ApplicationController
 	  @cellHeight =[1200/(@rows+1),100].max
 	  @unselectedColor = "rgb(180, 180, 180)"
 	  @selectedColor = "rgb(80, 80, 80)"
-	  @name = "David Liu's mock nomikai"
-	  @description="hoge"
 
 	  render "scheduler/main"
   end
