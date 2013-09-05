a9bdb677199e25e8cc6d8c4f15bb90ee28c73421
David Liu <david.shuang.liu@gmail.com>
2013-09-05 23:51:16 +0900

    Adding the new automatic time recommendation feature

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -1,7 +1,7 @@
 class SchedulerController < ApplicationController
   IDEALDATES=["1/1","1/2","1/3","1/4","1/5"]
   IDEALSTARTTIME=10.5
-  IDEALENDTIME=22
+  IDEALENDTIME=13
   def respondToToken
     @dates=IDEALDATES
     @s_time=IDEALSTARTTIME
@@ -9,13 +9,24 @@ class SchedulerController < ApplicationController
     @rows = (@e_time - @s_time) * 2 - 1
     @cols = @dates.length - 1
     @cellWidth = [800/(@cols+1),270].min
-    @cellHeight =[1200/(@rows+1),100].max
+    @cellHeight =[1200/(@rows+1),50].min
     @unselectedColor = "#DCDDDD"
     @selectedColor = "#339933"
     @name = "大江戸ハッカソン打ち上げパーティー"
     @description="hoge"
     
     @interestColor = "#586B29"
+    
+    # Prepare data and broadcast it via Websocket!
+    r = ((IDEALENDTIME - IDEALSTARTTIME) * 2).to_i
+    c = IDEALDATES.length
+    d = []
+    r.times do |i|
+      d << (1..c).map{rand(0..10)}
+    end
+    #d = (1..r*c).map{rand(0..10)}
+    result = JSON.generate({rows: r, cols: c, data: d})
+    WebsocketRails[:newTimes].trigger 'update',  result
 
     render "scheduler/main"
   end
